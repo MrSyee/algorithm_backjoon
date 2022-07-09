67cdcc613b683ba4e620c3825fb19e3098deded6
Kyunghwan kim <khsyee@gmail.com>
2022-07-09 19:47:13 +0900

    feat: Add 4963

---
+++ dfs/4963.py
@@ -0,0 +1,41 @@
+"""
+4963. 섬의 개수 (S2)
+https://www.acmicpc.net/problem/4963
+"""
+
+def solution():
+    import sys
+
+    sys.setrecursionlimit(10000)
+
+    def dfs(x, y, visited, whole_map):
+        visited[y][x] = True
+        dx = [1, 1, 0, -1, -1, -1, 0, 1]
+        dy = [0, 1, 1, 1, 0, -1, -1, -1]
+        for i in range(8):
+            tx = x + dx[i]
+            ty = y + dy[i]
+
+            if 0 <= tx < w and 0 <= ty < h:
+                if visited[ty][tx] == False and whole_map[ty][tx] == 1:
+                    dfs(tx, ty, visited, whole_map)
+
+    while True:
+        w, h = map(int, sys.stdin.readline().split())
+        if w == 0 and h == 0:
+            break
+
+        whole_map = list()
+        for _ in range(h):
+            whole_map.append(list(map(int, sys.stdin.readline().split())))
+        visited = [[False] * w for _ in range(h)]
+
+        count = 0
+        for y in range(h):
+            for x in range(w):
+                if visited[y][x] == False and whole_map[y][x] == 1:
+                    dfs(x, y, visited, whole_map)
+                    count += 1
+        print(count)
+
+solution()
